cc11aff2cc2ca8dd209eae28146a20e8b820eb32
Umberto Cicero <umbertoantonio.cicero@eng.it>
2017-01-11 22:09:46 +0100

    minaccia

--- execute.php
+++ execute.php
@@ -49,8 +49,9 @@ switch ($firstText) {
 	case "/testo":
 		$resultText  = "1. Ciao\n";
 		$resultText .= "2. Insulta {nome persona} - Es: Insulata Mario\n";
-		$resultText .= "3. Proverbio | Nonno\n";
-		$resultText .= "4. Poesia\n";
+		$resultText .= "3. Minaccia {nome persona} - Es: Minaccia Mario\n";
+		$resultText .= "4. Proverbio | Nonno\n";
+		$resultText .= "5. Poesia\n";
 		sendMsg($chatId,$resultText);
         break;
 	case "/foto":
