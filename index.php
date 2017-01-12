4142ba804dbaadfde7019b5ef5f3196d127d4586
umberto <umbertocicero@gmail.com>
2017-01-12 17:40:23 +0100

    a

--- index.php
+++ index.php
@@ -1,1 +1,22 @@
-<?php echo "Silence is golden";
+<!DOCTYPE html>
+<html>
+<body>
+
+<h1>CusenzBot</h1>
+
+<p>Questo bot é perfetto per tutti i cosentini (e non solo)</p>
+<br />
+<p>lista comandi per ogni categoria:</p>
+<ul>
+  <li>/testo</li>
+  <li>/foto</li>
+  <li>/meteo</li>
+</ul>
+ <br />
+ <p>Ideale in un gruppo di amici</p>
+<?php
+echo "-----";
+?> 
+
+</body>
+</html>
