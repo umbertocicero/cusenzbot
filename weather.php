3184ba1a05c5057d1b9d2d545f7022bb2ead4663
umberto <umbertocicero@gmail.com>
2017-01-13 16:07:18 +0100

    weather icon

--- weather.php
+++ weather.php
@@ -122,23 +122,22 @@ function getWeatherWeek(){
 			$datetime->setTimestamp($dt);
 			$la_time = new DateTimeZone('Europe/Rome');
 			$datetime->setTimezone($la_time);
-			//$j_time = $datetime->format('d-m-Y');
+			$j_time = $datetime->format('d-m-Y');
 			
 			setLocale(LC_TIME|LC_CTYPE, 'it_IT'); 
-		    $j_time = strftime("%A %d %m", $datetime);
+		    $day = strftime("%A ", $dt);
 			
 			
-			$result .= "Giorno ".$j_time." \n";
+			$result .= "Giorno ".$day.$j_time." \n";
 			
 			$temp = $weatherValue['temp']['day'];
 			$description = $weatherValue['weather'][0]['description'];
 			$mainType = $weatherValue['weather'][0]['main'];
-			$icon = getWeatherIco(mainType);
+			$icon = getWeatherIco($mainType);
 			
 			$result .= "Temperatura ".$temp."° \n";
 			$result .= ucfirst($description)." ".$icon." \n";
 			$result .= "\n";
-			$result .= $mainType."---".$icon;
 			
 		}
 	
@@ -176,5 +175,4 @@ function getWeatherIco($type){
 			break;
 	}
 	return json_decode($icon);
-	//return $icon;
 }
